80d339b6e25e8657ca7adf2992fbf469b6accd01
amiralitaherkhany <122179669+amiralitaherkhany@users.noreply.github.com>
2024-10-13 00:21:08 +0330

    created sign up screen and added lottie package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:crypto_app/providers/theme_provider.dart';
-import 'package:crypto_app/ui/main_wrapper.dart';
+import 'package:crypto_app/ui/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -46,7 +46,7 @@ class _MyMaterialAppState extends State<MyMaterialApp> {
           debugShowCheckedModeBanner: false,
           home: const Directionality(
             textDirection: TextDirection.ltr,
-            child: MainWrapper(),
+            child: SignUpScreen(),
           ),
         );
       },
---
+++ lib/ui/sign_up_screen.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+  bool _isObscure = true;
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.sizeOf(context).width;
+    var height = MediaQuery.sizeOf(context).height;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Lottie.asset(
+            'images/waveloop.json',
+            width: double.infinity,
+            height: height * 0.2,
+            fit: BoxFit.fill,
+          ),
+          SizedBox(
+            height: height * 0.01,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Text(
+              'Sign Up',
+              style: GoogleFonts.ubuntu(
+                fontSize: height * 0.035,
+                color: Theme.of(context).unselectedWidgetColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Text(
+              'Create an account',
+              style: GoogleFonts.ubuntu(
+                fontSize: height * 0.03,
+                color: Theme.of(context).unselectedWidgetColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: height * 0.03,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    textInputAction: TextInputAction.next,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your username';
+                      } else if (value.length < 4) {
+                        return 'Username must be at least 4 characters';
+                      } else if (value.length > 13) {
+                        return 'maximum character is 13';
+                      }
+                      return null;
+                    },
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        borderSide: BorderSide(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      prefixIcon: const Icon(Icons.person),
+                      hintText: 'Username',
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: height * 0.02,
+                  ),
+                  TextFormField(
+                    textInputAction: TextInputAction.next,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your gmail';
+                      } else if (!value.endsWith('@gmail.com')) {
+                        return 'please enter valid gmail';
+                      }
+                      return null;
+                    },
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        borderSide: BorderSide(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      prefixIcon: const Icon(Icons.email_rounded),
+                      hintText: 'gmail',
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: height * 0.02,
+                  ),
+                  TextFormField(
+                    textInputAction: TextInputAction.done,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      } else if (value.length < 7) {
+                        return 'Password must be at least 7 characters';
+                      } else if (value.length > 13) {
+                        return 'Password must be at most 13 characters';
+                      }
+                      return null;
+                    },
+                    controller: passwordController,
+                    obscureText: _isObscure,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.lock_open),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _isObscure = !_isObscure;
+                          });
+                        },
+                        icon: _isObscure
+                            ? const Icon(Icons.visibility_off)
+                            : const Icon(
+                                Icons.visibility,
+                              ),
+                      ),
+                      hintText: 'Password',
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        borderSide: BorderSide(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: height * 0.01,
+                  ),
+                  Text(
+                    'Creating an account means you\'re okay with our Terms of Services and our Privacy Policy',
+                    style: GoogleFonts.ubuntu(
+                      fontSize: 15,
+                      color: Colors.grey,
+                      height: 1.5,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: height * 0.02,
+                  ),
+                  signupBtn(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget signupBtn() {
+    return SizedBox(
+      width: double.infinity,
+      height: 55,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Theme.of(context).primaryColor,
+          foregroundColor: Theme.of(context).textTheme.bodySmall?.color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+        ),
+        onPressed: () {
+          // Validate returns true if the form is valid, or false otherwise.
+          // if all text form field validator cll backs returns null that means its validated
+          // if (_formKey.currentState!.validate()) {
+          //   userProvider.callRegisterApi(nameController.text, emailController.text, passwordController.text);
+          // }
+        },
+        child: Text(
+          'Sign Up',
+          style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                color: Colors.white,
+              ),
+        ),
+      ),
+    );
+  }
+}
